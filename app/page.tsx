d5fefeb928debb149030c2fe9f28296b02a4a3ab
Herb Caudill <herb@devresults.com>
2024-02-22 20:53:43 +0100

    app/page.tsx: terrible terrible terrible

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,22 @@
 'use client'
 
-import { next as A } from '@automerge/automerge'
+import { useEffect, useState } from 'react'
 
-export default function Home() {
-  const foo = A.init()
-  return <p>Hello, world</p>
+export default function PageComponent() {
+  const [message, setMessage] = useState<string | undefined>(undefined)
+  useEffect(() => {
+    import('@automerge/automerge').then(({ next: A }) => {
+      const foo = A.from({ message: 'Hello, world!' })
+      setMessage(foo.message)
+    })
+  }, [])
+
+  return <p>{message ?? 'Loading...'}</p>
 }
+
+// import { next as A } from '@automerge/automerge'
+
+// export default function Home() {
+//   const foo = A.init()
+//   return <p>Hello, world</p>
+// }
